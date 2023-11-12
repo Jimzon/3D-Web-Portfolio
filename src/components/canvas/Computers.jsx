470267ab860aea63bde8aca49273e07f244442de
Jimzon <104047846+Jimzon@users.noreply.github.com>
2023-11-12 11:30:01 +0800

    primitive

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,4 +1,4 @@
-import { Suspense, useEffect, useState } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
@@ -10,7 +10,12 @@ const Computers = () => {
     <mesh>
       <hemisphereLight intensity={0.15} groundColor="black" />
       <pointLight intensity={1} />
-      <primitive object={computer.scene} />
+      <primitive
+        object={computer.scene}
+        scale={0.75}
+        position={[0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+      />
     </mesh>
   );
 };
@@ -31,6 +36,8 @@ const ComputersCanvas = () => {
         />
         <Computers />
       </Suspense>
+
+      <Preload all />
     </Canvas>
   );
 };
